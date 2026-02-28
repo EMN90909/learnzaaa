7d1f7294dd35bf5328b547428013071ac52fd965
[dyad] <nasongoemmanuel8@gmail.com>
2026-02-28 03:22:06 +0300

    [dyad] Fixing useNavigate routing error - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 "use client";
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import LessonsPage from './pages/LessonsPage';
-import './globals.css'; // Import the correct CSS file
+import './globals.css';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/lessons" element={<LessonsPage />} />
-          <Route path="/lessons/new" element={<div>New Lesson Page</div>} />
-          <Route path="/settings" element={<div>Settings Page</div>} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/lessons" element={<LessonsPage />} />
+        <Route path="/lessons/new" element={<div>New Lesson Page</div>} />
+        <Route path="/settings" element={<div>Settings Page</div>} />
+      </Routes>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { SessionContextProvider } from "./integrations/supabase/supabaseContext";
 import "./globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-    <SessionContextProvider>
-      <App />
-    </SessionContextProvider>
+    <BrowserRouter>
+      <SessionContextProvider>
+        <App />
+      </SessionContextProvider>
+    </BrowserRouter>
   </ThemeProvider>
 );
