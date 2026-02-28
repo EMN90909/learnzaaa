d420e8add008d72f609a1b3080c22be3a719cab6
[dyad] <nasongoemmanuel8@gmail.com>
2026-02-28 03:19:36 +0300

    [dyad] Fixed SessionContextProvider wrapping issue in main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
+import { SessionContextProvider } from "./integrations/supabase/supabaseContext";
 import "./globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-    <App />
+    <SessionContextProvider>
+      <App />
+    </SessionContextProvider>
   </ThemeProvider>
 );
